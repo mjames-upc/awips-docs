0a6b597687fc6c8fb8a9a4da3c5d523da10dc6e6
Michael James <mjames@ucar.edu>
2017-04-14 08:10:13 -0600

    cleanup

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll'
-gem 'sass'
-gem 'octopress-hooks', git: 'https://github.com/octopress/hooks.git'
-gem 'octopress-paginate'
-gem 'compass'
-gem 'rouge'
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -f
-gem install bundler jekyll --user-install
-npm install grunt-cli vulcanize bower compass
-npm install grunt --save-dev
-bundle install
-npm install
-bower install
-grunt docs
-
